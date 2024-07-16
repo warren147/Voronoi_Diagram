de7e4dd4caaa46b4fae0c7d2d0284a4f7ff35fe8
Warren <warrenbluechg@gmail.com>
2024-07-15 18:57:21 -0700

    main gui

---
+++ VoronoiGUI.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class VoronoiGUI extends JFrame {
+
+    private final VoronoiPanel voronoiPanel;
+
+    public VoronoiGUI() {
+        setTitle("Voronoi Diagram Generator");
+        setSize(800, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        voronoiPanel = new VoronoiPanel();
+        add(voronoiPanel, BorderLayout.CENTER);
+
+        JButton generateButton = new JButton("Generate Voronoi");
+        generateButton.addActionListener(e -> voronoiPanel.generateVoronoi());
+        add(generateButton, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(VoronoiGUI::new);
+    }
+}
